b4a9b07e30ef0a55ad34233bf4ed0afbe4f37b69
Safi100 <safinafi12@gmail.com>
2025-05-31 19:57:39 +0300

    setup server

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const env = require("dotenv").config();
+const morgan = require("morgan");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const cron = require("node-cron");
+
+const app = express();
+
+mongoose
+  .connect(process.env.DATABASE)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Could not connect to MongoDB:", err));
+
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(8000, () => {
+  console.log("Server started on port 8000");
+});
