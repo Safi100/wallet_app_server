1035c7e420ece57bccefaa808d8ce951fba90927
Safi100 <safinafi12@gmail.com>
2025-06-02 00:28:54 +0300

    Create Category model

---
+++ models/category.model.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const categorySchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    icon: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Category", categorySchema);
