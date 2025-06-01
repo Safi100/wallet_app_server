aa52441cb8f9509f2402340935f5febdaf398dc7
Safi100 <safinafi12@gmail.com>
2025-06-02 00:28:17 +0300

    Create user model

---
+++ models/user.model.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", userSchema);
