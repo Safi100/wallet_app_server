e7468e381969bd80292710c3eb39b826dcaac42a
Safi100 <safinafi12@gmail.com>
2025-06-02 00:28:40 +0300

    Create transaction model

---
+++ models/transaction.model.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const transactionSchema = new Schema(
+  {
+    user_id: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    title: {
+      type: String,
+      required: false,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    category: {
+      type: Schema.Types.ObjectId,
+      ref: "Category",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Transaction", transactionSchema);
