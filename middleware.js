8dfc4e090175e6a615c10bc832cf0b0d72508470
Safi100 <safinafi12@gmail.com>
2025-06-05 21:49:51 +0300

    update middleware

--- middleware.js
+++ middleware.js
@@ -3,11 +3,25 @@ const HandleError = require("./utils/HandleError");
 
 module.exports.isLoggedIn = (req, res, next) => {
   try {
-    const token = req.headers.authorization || req.cookies.access_token;
-    if (!token) throw new HandleError("You must log in to access this", 401);
+    let token = null;
+
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith("Bearer ")
+    ) {
+      token = req.headers.authorization.split(" ")[1];
+    }
+
+    if (!token && req.cookies?.access_token) {
+      token = req.cookies.access_token;
+    }
+
+    if (!token) {
+      throw new HandleError("You must log in to access this", 401);
+    }
 
     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-      if (err) throw new HandleError("Invalid token", 401);
+      if (err) throw new HandleError("Invalid or expired token", 401);
       req.user = decoded;
       next();
     });
